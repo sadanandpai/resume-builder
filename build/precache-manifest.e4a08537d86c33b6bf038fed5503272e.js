e86f00dc1fd31b9155d83316cd469cdc76ac8ef6
Sadanand Pai <sadypai@gmail.com>
2021-01-23 07:05:08 +0000

    build files

---
+++ build/precache-manifest.e4a08537d86c33b6bf038fed5503272e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb44a7c0bd4f120118a42699370ba7f",
+    "url": "/single-page-resume-builder/index.html"
+  },
+  {
+    "revision": "1b010f0afc82f0d047f6",
+    "url": "/single-page-resume-builder/static/css/main.8b3d0078.chunk.css"
+  },
+  {
+    "revision": "58f5af22f767a42aee9b",
+    "url": "/single-page-resume-builder/static/js/2.20facd8c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/single-page-resume-builder/static/js/2.20facd8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b010f0afc82f0d047f6",
+    "url": "/single-page-resume-builder/static/js/main.0e501218.chunk.js"
+  },
+  {
+    "revision": "8b5293c9cd64149f5a40",
+    "url": "/single-page-resume-builder/static/js/runtime-main.fbc203bd.js"
+  }
+]);
