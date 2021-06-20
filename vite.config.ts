f4ff8741ad570545137627378321f25d2438cf0e
Sadanand Akshay Pai <sadypai@gmail.com>
2021-06-20 14:48:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  build: {
+    target: 'es2018'
+  }
 })
