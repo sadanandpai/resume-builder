6657a4f29392c1ae92cdc48149d7e3c55641eb6b
Gopalakrishnan <krishnan.gopal34@gmail.com>
2021-07-03 15:29:20 +0530

    Flex style resued

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import styled from "styled-components";
 import { Resume } from './Resume'
 import { Sidebar } from './Sidebar'
-
-const HomeWrapper = styled.div`
-    display: flex;
-`;
+import { Flex } from "../common/styles"
 
 export const Home = () => {
     return (
-        <HomeWrapper>
+        <Flex>
             <Resume />
             <Sidebar />
-        </HomeWrapper>
+        </Flex>
     )
 }
