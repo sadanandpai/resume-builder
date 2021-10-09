86d59afe80505715267bf417e2b95f96d23cb0eb
Sadanand Pai <sadanand.pai@coindcx.com>
2021-10-09 16:59:27 +0530

    Side bar fix & redundancy removal

--- src/components/core/settings-header/TemplatesSettings.tsx
+++ src/components/core/settings-header/TemplatesSettings.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import { Popover, Button } from "antd";
+import React from "react";
+import { Button } from "antd";
 import styled from "styled-components";
 import { useTemplates } from "../../stores/settings.store";
 import Professional from "../../assets/professional.jpg";
 import Legacy from "../../assets/legacy.jpg";
 
-const TemplateRadio = styled.input.attrs({ type: "radio" })`
-  position: absolute;
-  opacity: 0;
-  cursor: pointer;
+const TemplateWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+  grid-gap: 1rem;
 `;
 
 const TemplateThumbnailImg = styled.img`
@@ -33,40 +33,22 @@ const TemplateThumbnail = styled.label`
   user-select: none;
   transition: all 0.3s;
 
-  ${TemplateRadio}:checked ~ ${TemplateThumbnailImg} {
+  .selected{
     border: solid 2px #1890ff;
   }
 `;
 
-const TemplateWrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, minmax(0, 1fr));
-  grid-gap: 1rem;
-`;
-
 export function TemplateSettings() {
+  const templateIndex = useTemplates((state: any) => state.index);
   const setTemplate = useTemplates((state: any) => state.setTemplate);
-
-  const content = (
-    <div>
-      <Button onClick={() => setTemplate(0)}>0</Button>
-      <Button onClick={() => setTemplate(1)}>1</Button>
-    </div>
-  );
 
   return (
     <TemplateWrapper>
       <TemplateThumbnail>
-        <TemplateRadio type="radio" id="" name="template" checked={true} />
-
-        <TemplateThumbnailImg src={Professional} alt="Professional" />
-        {/* <span>Professional</span> */}
+        <TemplateThumbnailImg src={Professional} alt="Professional" className={templateIndex === 0 ? 'selected' : ''} onClick={() => setTemplate(0)} />
       </TemplateThumbnail>
       <TemplateThumbnail>
-        <TemplateRadio type="radio" id="" name="template" />
-
-        <TemplateThumbnailImg src={Legacy} alt="Legacy" />
-        {/* <span>Legacy</span> */}
+        <TemplateThumbnailImg src={Legacy} alt="Legacy" className={templateIndex === 1 ? 'selected' : ''} onClick={() => setTemplate(1)} />
       </TemplateThumbnail>
     </TemplateWrapper>
   );
