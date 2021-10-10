6380c56daf78ce82f772055d7984d9d12b9cbe1d
Gopalakrishnan <krishnan.gopal34@gmail.com>
2021-10-10 16:48:04 +0530

    refactor: :art: Moved common code to new component
    Moved Sidebar common code to new component called side menu as a resuabe component

--- src/components/pages/Sidebar.tsx
+++ src/components/pages/Sidebar.tsx
@@ -1,40 +1,10 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { Tooltip } from "antd";
-import { getIcon } from "../common/icons";
 
 import { SideDrawer } from "../widgets/SideDrawer";
 import { TemplateSettings } from "../core/settings-header/TemplatesSettings";
-import { EditSettings } from "../core/settings-header/EditSettings";
 import { ThemeSettings } from "../core/settings-header/ThemeSettings";
-
-const Sider = styled.nav`
-  height: 100%;
-  font-size: 1.4rem;
-  padding: 8px;
-  background: #222;
-`;
-
-const IconWrapper = styled.div`
-  outline-color: transparent;
-  margin-bottom: 1rem;
-`;
-
-const IconButton = styled.button`
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  cursor: pointer;
-  justify-content: center;
-  align-items: center;
-  height: 36px;
-  width: 40px;
-  background: transparent;
-  border: 0;
-  border-radius: 2px;
-  padding: 0;
-  color: rgb(230, 230, 230);
-`;
+import { SideMenu } from "../widgets/SideMenu";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -44,54 +14,36 @@ const Wrapper = styled.div`
 const sideBarList = [
   {
     key: 0,
-    title: "Edit",
-    icon: "edit",
-    component: <EditSettings />
+    title: "Theme",
+    icon: "color",
+    component: <ThemeSettings />,
   },
   {
     key: 1,
-    title: "Theme",
-    icon: "color",
-    component: <ThemeSettings />
+    title: "Template",
+    icon: "template",
+    component: <TemplateSettings />,
   },
   {
     key: 2,
-    title: "Template",
-    icon: "template",
-    component: <TemplateSettings />
-  },
-  {
-    key: 3,
     title: "Download",
     icon: "download",
-    component: <TemplateSettings />
+    component: <TemplateSettings />,
   },
 ];
 
 export const Sidebar = () => {
   const [activeTab, setActiveTab] = useState(-1);
 
-  function clickHandler(event: any){
-    if(activeTab === event.currentTarget.dataset.id)
-      setActiveTab(-1);
-    else
-      setActiveTab(event.currentTarget.dataset.id);
+  function clickHandler(event: any) {
+    if (activeTab === event.currentTarget.dataset.id) setActiveTab(-1);
+    else setActiveTab(event.currentTarget.dataset.id);
   }
 
   return (
     <Wrapper>
-      <SideDrawer isShown={activeTab !== -1}>
-        {sideBarList[activeTab]?.component}
-      </SideDrawer>
-      <Sider>
-        {sideBarList.map((item) => (
-          <IconWrapper key={item.key} data-id={item.key} onClick={clickHandler}>
-            <Tooltip placement="left" title={item.title}>
-              <IconButton>{getIcon(`${item.icon}`)}</IconButton>
-            </Tooltip>
-          </IconWrapper>
-        ))}
-      </Sider>
+      <SideDrawer isShown={activeTab !== -1}>{sideBarList[activeTab]?.component}</SideDrawer>
+      <SideMenu menuList={sideBarList} onClick={clickHandler} />
     </Wrapper>
   );
 };
