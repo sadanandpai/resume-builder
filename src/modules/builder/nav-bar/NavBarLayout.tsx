8f0c32f5539d437d6f68cf2be706e5db95ca0852
sivak <sivakwalker3@gmail.com>
2022-04-15 16:20:07 +0530

    update: 🚧 navbar customized

--- src/modules/builder/nav-bar/NavBarLayout.tsx
+++ src/modules/builder/nav-bar/NavBarLayout.tsx
@@ -1,18 +1,19 @@
 import Image from 'next/image';
 
-import { NavBarMenu } from './atoms/NavBarMenu';
-import { NavBarActions } from './atoms/NavBarActions';
 import { CustomizedButton } from 'src/common/components';
+
+import { NavBarMenu, NavBarActions } from './atoms';
+import { NavMenuItem } from './components/MenuItem';
+import { ThemeSelectPopover } from './components/ThemeSelect';
 
 const NavBarLayout = () => {
   return (
     <nav className="h-14 w-full bg-resume-800 relative flex py-2.5 px-4 items-center">
       <Image src={'/icons/right-arrow.svg'} alt="logo" height={'39px'} width={'32px'} />
-
       <div className="flex-auto flex justify-between items-center ml-5">
         <NavBarMenu>
-          <h1 className="text-resume-50">Templates</h1>
-          <h1 className="text-resume-50">Themes</h1>
+          <NavMenuItem caption="Templates" popoverChildren={<ThemeSelectPopover />} />
+          <NavMenuItem caption="Templates" popoverChildren={<ThemeSelectPopover />} />
         </NavBarMenu>
         <NavBarActions>
           <CustomizedButton variant="text" color="resume50">
