49c539bcfc66945a3b3a99ac9315f87ed711793e
Sadanand Pai <sadanand.pai@NTPL-ITMACLAP0235.local>
2022-04-26 00:14:02 +0530

    Visibility toggle for skills

--- src/helpers/common/components/EditSectionContainer.tsx
+++ src/helpers/common/components/EditSectionContainer.tsx
@@ -1,4 +1,18 @@
+import { motion, AnimatePresence } from 'framer-motion';
 import Image from 'next/image';
+import { useState } from 'react';
+
+const animation = {
+  exit: {
+    height: '0',
+    paddingTop: 0,
+    paddingBottom: 0,
+    opacity: 0,
+  },
+};
+
+const nonEditableStyle =
+  "opacity-60 after:content-[''] after:absolute after:h-full after:w-full after:top-0 after:left-0 after:cursor-not-allowed";
 
 const EditSectionContainer = ({
   title,
@@ -11,18 +25,40 @@ const EditSectionContainer = ({
   children: JSX.Element;
   clickHandler: () => void;
 }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const toggleVisibility = (e: React.MouseEvent) => {
+    setIsVisible(!isVisible);
+    e.stopPropagation();
+  };
+
   return (
     <div>
       <div
-        className="bg-resume-100 h-12 w-full rounded-t-md shadow-level-4px-0.25 relative flex items-center justify-between px-4 text-resume-800 font-bold text-lg select-none cursor-pointer"
+        className="bg-resume-100 h-12 w-full rounded-t-md shadow-level-4px-0.25 relative flex items-center justify-between px-4 text-resume-800 font-bold text-lg select-none cursor-pointer z-10"
         onClick={clickHandler}
       >
         <span>{title}</span>
-        <Image src="/icons/eye.svg" alt="eye" height="16px" width="21px" />
+        <Image
+          src={isVisible ? '/icons/eye.svg' : '/icons/eye-slash.svg'}
+          alt="eye"
+          height="16px"
+          width="21px"
+          onClick={toggleVisibility}
+        />
       </div>
-      {expanded && (
-        <div className="bg-resume-10 rounded-b-md px-4 py-6 shadow-level-4px-0.25">{children}</div>
-      )}
+      <AnimatePresence>
+        {expanded && (
+          <motion.div
+            className={`bg-resume-10 relative rounded-b-md px-4 py-6 shadow-level-4px-0.25 overflow-hidden ${
+              !isVisible && nonEditableStyle
+            }`}
+            exit={animation.exit}
+          >
+            {children}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
