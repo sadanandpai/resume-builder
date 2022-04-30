57ddb295dcf9871c4535f687218d8e1c6e5516fd
sivak <sivakwalker3@gmail.com>
2022-04-30 09:13:11 +0530

    feat: 💥 datepicker mui library inclusion in dependency

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers';
 
 import { GLOBAL_MUI_THEME } from '../styles/global.theme';
 
@@ -16,7 +18,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <CacheProvider value={cache}>
       <ThemeProvider theme={GLOBAL_MUI_THEME}>
-        <Component {...pageProps} />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <Component {...pageProps} />
+        </LocalizationProvider>
       </ThemeProvider>
     </CacheProvider>
   );
