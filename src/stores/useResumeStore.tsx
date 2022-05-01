bf42f1b7707ee324a640c1a31dc24693e80d77bd
Sadanand Pai <sadanand.pai@NTPL-ITMACLAP0235.local>
2022-05-01 12:03:58 +0530

    useResumeStore custom hook

---
+++ src/stores/useResumeStore.tsx
@@ -0,0 +1,26 @@
+import ResumeData from 'src/helpers/constants/resume-data.json';
+
+import {
+  useDatabases,
+  useFrameworks,
+  useLanguages,
+  useLibraries,
+  usePractices,
+  useTechnologies,
+  useTools,
+} from 'src/stores/skills';
+
+export const useResumeStore = () => {
+  return {
+    ...ResumeData,
+    skills: {
+      languages: useLanguages((state) => state.get()),
+      frameworks: useFrameworks((state) => state.get()),
+      technologies: useTechnologies((state) => state.get()),
+      libraries: useLibraries((state) => state.get()),
+      databases: useDatabases((state) => state.get()),
+      practices: usePractices((state) => state.get()),
+      tools: useTools((state) => state.get()),
+    },
+  };
+};
