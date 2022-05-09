7e55ff508707974ebeef59a02ecc0ffd50d6648c
Vivek <audit536@gmail.com>
2022-05-09 21:01:10 +0530

    feat : :sparkles: Add home page second section

--- src/modules/home/HomeLayout.tsx
+++ src/modules/home/HomeLayout.tsx
@@ -7,10 +7,13 @@ import 'animate.css';
 const HomeLayout = () => {
   return (
     <>
-      <div className="bg-resume-50 animate__animated animate__zoomIn">
-        <nav className="h-14 w-full bg-resume-800 relative flex py-2.5 pl-40 pr-4 items-center">
+      <div style={{ background: 'linear-gradient(180deg, #E7EEFA 50%, #FFFFFF 100%);' }}>
+        <nav className="h-14 w-full bg-resume-800 relative flex py-2.5 px-60 items-center">
           <Image src={'/icons/resume-icon.svg'} alt="logo" height={'36px'} width={'36px'} />
           <div className="flex-auto flex justify-between items-center ml-5">
+            <NavBarActions>
+              <StyledButton variant="text">Editor</StyledButton>
+            </NavBarActions>
             <NavBarActions>
               <StyledButton variant="text">Contribute</StyledButton>
               <StyledButton variant="text">About us</StyledButton>
@@ -19,28 +22,26 @@ const HomeLayout = () => {
           </div>
         </nav>
 
-        <div className="mx-32 my-6">
+        <div className="mx-56 my-6">
           <div className="flex">
-            {/* <img src={'/icons/resume.png'} alt="resume-3d" style={{width : "380px"}} /> */}
-            <Image
+            <img
               src={'/icons/resume.png'}
               alt="resume-3d"
-              height={'380px'}
-              width={'380px'}
-              unoptimized={true}
+              style={{ width: '380px', height: 'auto' }}
+              className="animate__animated animate__headShake animate__delay-1s"
             />
-            <div className="mt-12">
+            <div className="mt-12 -ml-12">
               <h3
                 className="text-2xl mb-2 text-resume-400"
                 style={{ fontFamily: "'Roboto Condensed', serif" }}
               >
-                SIMPLEST WAY TO BUILD
+                SIMPLEST WAY TO BUILD A
               </h3>
               <h1
                 className="text-5xl mb-12 text-resume-800"
                 style={{ fontFamily: "'Roboto Slab', serif" }}
               >
-                Professional Resumes
+                Professional Resume
               </h1>
 
               <div className="flex mb-10">
@@ -67,6 +68,47 @@ const HomeLayout = () => {
                 </Button>
               </Link>
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mx-60 my-32" style={{ fontFamily: "'Roboto Slab', serif" }}>
+        <div className="grid grid-cols-2 gap-6">
+          <div
+            className="bg-resume-100 px-14 py-12 rounded-lg flex justify-between shadow-lg"
+            style={{ borderBottomRightRadius: '32px' }}
+          >
+            <Image src={'/icons/style.svg'} alt="logo" height={'64px'} width={'64px'} />
+            <p className="text-resume-800 text-xl ml-8">
+              With <strong>templates and themes</strong>, make it yours!
+            </p>
+          </div>
+          <div
+            className="bg-resume-100 px-14 py-12 rounded-lg flex justify-between shadow-lg"
+            style={{ borderBottomLeftRadius: '32px' }}
+          >
+            <p className="text-resume-800 text-xl">
+              Go straight to building, no <strong>sign ups</strong>
+            </p>
+            <Image src={'/icons/magic.svg'} alt="logo" height={'64px'} width={'64px'} />
+          </div>
+          <div
+            className="bg-resume-100 px-14 py-12 rounded-lg flex justify-between shadow-lg"
+            style={{ borderTopRightRadius: '32px' }}
+          >
+            <Image src={'/icons/code.svg'} alt="logo" height={'64px'} width={'64px'} />
+            <p className="text-resume-800 text-xl ml-8">
+              We love <strong>open source</strong>, we hope you too!
+            </p>
+          </div>
+          <div
+            className="bg-resume-100 px-14 py-12 rounded-lg flex justify-between shadow-lg"
+            style={{ borderTopLeftRadius: '32px' }}
+          >
+            <p className="text-resume-800 text-xl">
+              Your <strong>data</strong> never leaves your device
+            </p>
+            <Image src={'/icons/lock.svg'} alt="logo" height={'64px'} width={'64px'} />
           </div>
         </div>
       </div>
