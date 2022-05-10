5fb0cc23c0da338ec9b3b26d3a08c4ddfc81560f
Vivek <audit536@gmail.com>
2022-05-11 02:00:06 +0530

    ui : :art: Make first and last section responsive

--- src/modules/home/HomeLayout.tsx
+++ src/modules/home/HomeLayout.tsx
@@ -23,16 +23,16 @@ const HomeLayout = () => {
           </div>
         </nav>
 
-        <div className="mx-4 md:mx-40 xl:mx-60 my-6">
-          <div className="flex flex-row mt-24">
-            <div className="basis-full md:basis-3/12 md:mr-24">
+        <div className="mx-6 md:mx-40 xl:mx-60 my-6">
+          <div className="grid grid-cols-12 mt-12 md:mt-24">
+            <div className="col-span-12 sm:col-span-4">
               <img
                 src={'/icons/resume.png'}
                 alt="resume-3d"
-                className="animate__animated animate__headShake animate__delay-1s"
+                className="animate__animated animate__headShake animate__delay-1s w-6/12 sm:w-9/12"
               />
             </div>
-            <div className="basis-full md:basis-9/12">
+            <div className="col-span-12 sm:col-span-8">
               <h3
                 className="text-2xl mb-2 text-resume-400"
                 style={{ fontFamily: "'Roboto Condensed', serif" }}
@@ -75,10 +75,11 @@ const HomeLayout = () => {
       </div>
 
       <div
-        className="mx-4 md:mx-40 xl:mx-60 my-32 w-75"
+        className="mx-6 md:mx-40 xl:mx-60 my-32 w-75"
         style={{ fontFamily: "'Roboto Slab', serif" }}
       >
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          {/* These cards are written in JSX because I do not know Typescript */}
           <FeatureCard number={1} />
           <FeatureCard number={2} />
           <FeatureCard number={3} />
@@ -86,29 +87,29 @@ const HomeLayout = () => {
         </div>
       </div>
 
-      <div className="mx-4 md:mx-40 xl:mx-60 my-32" style={{ fontFamily: "'Roboto Slab', serif" }}>
-        <div className="flex items-center justify-center">
-          <div className="basis-3/12 flex-col text-center">
+      <div className="mx-6 md:mx-40 xl:mx-60 my-32" style={{ fontFamily: "'Roboto Slab', serif" }}>
+        <div className="grid grid-cols-12 items-center text-center">
+          <div className="col-span-12 lg:col-span-4 mb-4 lg:mb-0">
             <Image src={'/icons/palette.svg'} alt="logo" height={'48px'} width={'48px'} />
-            <p className="text-resume-800 text-xl mt-4">
+            <p className="text-resume-800 text-xl mt-2">
               Do you want to make your own <strong>template?</strong>
             </p>
           </div>
-          <div className="basis-1/12 flex-col text-center text-4xl">
+          <div className="col-span-12 lg:col-span-1 mb-4 lg:mb-0 text-resume-800 text-4xl">
             <p>+</p>
           </div>
-          <div className="basis-2/12 px-2 flex-col text-center">
+          <div className="col-span-12 lg:col-span-2 flex-col">
             <Image src={'/icons/terminal.svg'} alt="logo" height={'48px'} width={'48px'} />
-            <p className="text-resume-800 text-xl mt-4">
+            <p className="text-resume-800 text-xl mt-2">
               Do you write <strong>React</strong> code?
             </p>
           </div>
-          <div className="basis-1/12 flex-col text-center text-4xl mx-6">
+          <div className="invisible lg:visible lg:col-span-2 text-resume-800 text-4xl mx-6">
             <p>=</p>
           </div>
-          <div className="basis-2/12 px-2 flex-col text-center">
+          <div className="col-span-12 lg:col-span-3 mx-auto">
             <Image src={'/icons/wave.svg'} alt="logo" height={'48px'} width={'48px'} />
-            <Button variant="contained" className="bg-resume-800 mt-7 mb-3">
+            <Button variant="contained" className="block bg-resume-800 mt-2 lg:mt-5 mb-3">
               CONTRIBUTE
             </Button>
           </div>
