9b3487faf3831691164daf56db44d8b31a25ba0d
Vivek <audit536@gmail.com>
2022-05-20 19:09:54 +0530

    ui: :art: Add new hover state and animation to each card

--- src/modules/home/components/FeatureCard.jsx
+++ src/modules/home/components/FeatureCard.jsx
@@ -4,38 +4,51 @@ function FeatureCard(props) {
   return (
     <>
       {props.number == 1 && (
-        <div className="bg-resume-100 hover:bg-resume-200 px-6 lg:px-14 py-6 lg:py-12 rounded-lg flex justify-between items-center shadow-lg lg:rounded-br-[32px]">
-          <Image src={'/icons/style.svg'} alt="logo" height={'64px'} width={'64px'} />
-          <p className="text-resume-800 text-xl ml-8 basis-10/12">
-            With <strong>templates and themes</strong>, make your own resume!
+        <div className="transition ease-in-out delay-75 bg-resume-100 hover:bg-resume-600 text-resume-800 hover:text-resume-50 fill-resume-800 hover:fill-resume-100 px-6 lg:px-14 py-6 lg:py-12 rounded-lg flex justify-between items-center shadow-lg lg:rounded-br-[32px]">
+          {/* <Image src={'/icons/style.svg'} alt="logo" height={'64px'} width={'64px'} /> */}
+          <svg width="64" height="64" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.1333 54.3333L8.59999 53.8667C7.17777 53.2444 6.2111 52.1778 5.69999 50.6667C5.18888 49.1555 5.33333 47.6667 6.13333 46.2L10.1333 37.1333V54.3333ZM22.5333 59.5333C21.0222 59.5333 19.7333 59 18.6667 57.9333C17.6 56.8667 17.0667 55.5778 17.0667 54.0667V40.2667L23.2 57.7333C23.2889 58.0889 23.4222 58.4 23.6 58.6667C23.7778 58.9333 24 59.2222 24.2667 59.5333H22.5333ZM35.0667 57.9333C34.1333 58.3333 33.1556 58.2444 32.1333 57.6667C31.1111 57.0889 30.3778 56.2889 29.9333 55.2667L17.2 20.6667C16.7555 19.6444 16.8 18.6444 17.3333 17.6667C17.8667 16.6889 18.6889 15.9778 19.8 15.5333L41 7.86665C42.0222 7.5111 43.0222 7.59999 44 8.13332C44.9778 8.66665 45.6889 9.46665 46.1333 10.5333L58.8667 44.7333C59.1778 45.8444 59.1111 46.9333 58.6667 48C58.2222 49.0667 57.4444 49.8 56.3333 50.2L35.0667 57.9333ZM29.6 24.9333C30.1333 24.9333 30.6 24.7333 31 24.3333C31.4 23.9333 31.6 23.4667 31.6 22.9333C31.6 22.4 31.4 21.9333 31 21.5333C30.6 21.1333 30.1333 20.9333 29.6 20.9333C29.0667 20.9333 28.6 21.1333 28.2 21.5333C27.8 21.9333 27.6 22.4 27.6 22.9333C27.6 23.4667 27.8 23.9333 28.2 24.3333C28.6 24.7333 29.0667 24.9333 29.6 24.9333Z" />
+          </svg>
+          <p className="text-xl ml-8 basis-10/12">
+            Customise with <strong>templates and themes</strong>!
           </p>
         </div>
       )}
 
       {props.number == 2 && (
-        <div className="bg-resume-100 hover:bg-resume-200 px-6 lg:px-14 py-6 lg:py-12 rounded-lg flex justify-between items-center shadow-lg lg:rounded-bl-[32px]">
-          <p className="text-resume-800 text-xl mr-8 basis-10/12">
+        <div className="transition ease-in-out delay-75 bg-resume-100 hover:bg-resume-600 text-resume-800 hover:text-resume-50 fill-resume-800 hover:fill-resume-100 px-6 lg:px-14 py-6 lg:py-12 rounded-lg flex justify-between items-center shadow-lg lg:rounded-bl-[32px]">
+          <p className="text-xl mr-8 basis-10/12">
             Well designed <strong>editor</strong> that helps you build easily
           </p>
-          <Image src={'/icons/speed.svg'} alt="logo" height={'64px'} width={'64px'} />
+          {/* <Image src={'/icons/speed.svg'} alt="logo" height={'64px'} width={'64px'} /> */}
+          <svg width="48" height="48" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.15 24.75L6.9 22C6.43333 21.8 6.15 21.4417 6.05 20.925C5.95 20.4084 6.08333 19.9667 6.45 19.6L13.25 12.75C13.7167 12.2834 14.275 11.95 14.925 11.75C15.575 11.55 16.2333 11.5167 16.9 11.65L20.75 12.4C18.9167 14.4667 17.4333 16.425 16.3 18.275C15.1667 20.125 14.1167 22.2834 13.15 24.75ZM42.15 4.85003C42.35 4.88336 42.55 4.9417 42.75 5.02503C42.95 5.10836 43.1333 5.23336 43.3 5.40003C43.4667 5.5667 43.5917 5.7417 43.675 5.92503C43.7583 6.10836 43.8167 6.30003 43.85 6.50003C44.0833 9.6667 43.4667 12.9667 42 16.4C40.5333 19.8334 38.45 22.9 35.75 25.6C33.95 27.4 32.1167 28.925 30.25 30.175C28.3833 31.425 26.4 32.4334 24.3 33.2C23.9333 33.3667 23.575 33.425 23.225 33.375C22.875 33.325 22.6 33.2 22.4 33L15.65 26.25C15.45 26.05 15.325 25.775 15.275 25.425C15.225 25.075 15.2833 24.7167 15.45 24.35C16.25 22.25 17.275 20.2584 18.525 18.375C19.775 16.4917 21.2833 14.6667 23.05 12.9C25.75 10.2 28.8167 8.12503 32.25 6.67503C35.6833 5.22503 38.9833 4.6167 42.15 4.85003ZM29.3 19.35C29.9667 20.0167 30.7917 20.35 31.775 20.35C32.7583 20.35 33.5833 20.0167 34.25 19.35C34.9167 18.6834 35.25 17.8584 35.25 16.875C35.25 15.8917 34.9167 15.0667 34.25 14.4C33.5833 13.7334 32.7583 13.4 31.775 13.4C30.7917 13.4 29.9667 13.7334 29.3 14.4C28.6333 15.0667 28.3 15.8917 28.3 16.875C28.3 17.8584 28.6333 18.6834 29.3 19.35ZM23.95 35.5C26.4167 34.5334 28.575 33.4834 30.425 32.35C32.275 31.2167 34.2333 29.7334 36.3 27.9L37 31.75C37.1333 32.4167 37.1 33.075 36.9 33.725C36.7 34.375 36.3667 34.9334 35.9 35.4L29.1 42.2C28.7333 42.5667 28.2833 42.7 27.75 42.6C27.2167 42.5 26.85 42.2167 26.65 41.75L23.95 35.5ZM8.1 32.1C9.26667 30.9334 10.6833 30.3417 12.35 30.325C14.0167 30.3084 15.4333 30.8834 16.6 32.05C17.7667 33.2167 18.35 34.6334 18.35 36.3C18.35 37.9667 17.7667 39.3834 16.6 40.55C15.7333 41.4167 14.3833 42.1334 12.55 42.7C10.7167 43.2667 7.98333 43.8 4.35 44.3C4.85 40.6667 5.38333 37.9417 5.95 36.125C6.51667 34.3084 7.23333 32.9667 8.1 32.1Z" />
+          </svg>
         </div>
       )}
 
       {props.number == 3 && (
-        <div className="bg-resume-100 hover:bg-resume-200 px-6 lg:px-14 py-6 lg:py-12 rounded-lg flex justify-between items-center shadow-lg lg:rounded-tr-[32px]">
-          <Image src={'/icons/magic.svg'} alt="logo" height={'64px'} width={'64px'} />
-          <p className="text-resume-800 text-xl ml-8 basis-10/12">
+        <div className="transition ease-in-out delay-75 bg-resume-100 hover:bg-resume-600 text-resume-800 hover:text-resume-50 fill-resume-800 hover:fill-resume-100 px-6 lg:px-14 py-6 lg:py-12 rounded-lg flex justify-between items-center shadow-lg lg:rounded-tr-[32px]">
+          {/* <Image src={'/icons/magic.svg'} alt="logo" height={'64px'} width={'64px'} /> */}
+          <svg width="64" height="64" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+            <path d="M25.6 48.5333L19.2667 34.6L5.33337 28.2667L19.2667 21.9333L25.6 8L31.9334 21.9333L45.8667 28.2667L31.9334 34.6L25.6 48.5333ZM48.5334 56L45.4 49L38.4 45.8667L45.4 42.6667L48.5334 35.7333L51.7334 42.6667L58.6667 45.8667L51.7334 49L48.5334 56Z" />
+          </svg>
+
+          <p className="text-xl ml-8 basis-10/12">
             Go straight to building, no <strong>sign up </strong>needed
           </p>
         </div>
       )}
 
       {props.number == 4 && (
-        <div className="bg-resume-100 hover:bg-resume-200 px-6 lg:px-14 py-6 lg:py-12 rounded-lg flex justify-between items-center shadow-lg lg:rounded-tl-[32px]">
-          <p className="text-resume-800 text-xl mr-8 basis-10/12">
+        <div className="transition ease-in-out delay-75 bg-resume-100 hover:bg-resume-600 text-resume-800 hover:text-resume-50 fill-resume-800 hover:fill-resume-100 px-6 lg:px-14 py-6 lg:py-12 rounded-lg flex justify-between items-center shadow-lg lg:rounded-tl-[32px]">
+          <p className="text-xl mr-8 basis-10/12">
             Your <strong>data</strong> never leaves your device
           </p>
-          <Image src={'/icons/lock.svg'} alt="logo" height={'64px'} width={'64px'} />
+          {/* <Image src={'/icons/lock.svg'} alt="logo" height={'64px'} width={'64px'} /> */}
+          <svg width="64" height="64" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.6666 58.6666C13.5555 58.6666 12.6111 58.2777 11.8333 57.5C11.0555 56.7222 10.6666 55.7777 10.6666 54.6666V25.7333C10.6666 24.6222 11.0555 23.6777 11.8333 22.9C12.6111 22.1222 13.5555 21.7333 14.6666 21.7333H19.3333V15.3333C19.3333 11.8222 20.5666 8.83329 23.0333 6.36663C25.5 3.89996 28.4888 2.66663 32 2.66663C35.5111 2.66663 38.5 3.89996 40.9666 6.36663C43.4333 8.83329 44.6666 11.8222 44.6666 15.3333V21.7333H49.3333C50.4444 21.7333 51.3888 22.1222 52.1666 22.9C52.9444 23.6777 53.3333 24.6222 53.3333 25.7333V54.6666C53.3333 55.7777 52.9444 56.7222 52.1666 57.5C51.3888 58.2777 50.4444 58.6666 49.3333 58.6666H14.6666ZM23.3333 21.7333H40.6666V15.3333C40.6666 12.9333 39.8222 10.8888 38.1333 9.19996C36.4444 7.51107 34.4 6.66663 32 6.66663C29.6 6.66663 27.5555 7.51107 25.8666 9.19996C24.1777 10.8888 23.3333 12.9333 23.3333 15.3333V21.7333ZM32 50.8666C32.5777 50.8666 33.0555 50.6777 33.4333 50.3C33.8111 49.9222 34 49.4444 34 48.8666V41.8666H41C41.5777 41.8666 42.0555 41.6777 42.4333 41.3C42.8111 40.9222 43 40.4444 43 39.8666C43 39.2888 42.8111 38.8111 42.4333 38.4333C42.0555 38.0555 41.5777 37.8666 41 37.8666H34V30.8666C34 30.2888 33.8111 29.8111 33.4333 29.4333C33.0555 29.0555 32.5777 28.8666 32 28.8666C31.4222 28.8666 30.9444 29.0555 30.5666 29.4333C30.1888 29.8111 30 30.2888 30 30.8666V37.8666H23C22.4222 37.8666 21.9444 38.0555 21.5666 38.4333C21.1888 38.8111 21 39.2888 21 39.8666C21 40.4444 21.1888 40.9222 21.5666 41.3C21.9444 41.6777 22.4222 41.8666 23 41.8666H30V48.8666C30 49.4444 30.1888 49.9222 30.5666 50.3C30.9444 50.6777 31.4222 50.8666 32 50.8666Z" />
+          </svg>
         </div>
       )}
     </>
