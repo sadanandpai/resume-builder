2d236cef09af96582529e02475207832379aff24
Siva K <sivakwalker3@gmail.com>
2022-06-04 23:56:25 +0530

    feat: 🎉 awards store init

---
+++ src/stores/awards.interface.ts
@@ -0,0 +1,16 @@
+export interface AwardItem {
+  title: string;
+  awarder: string;
+  date: string | null;
+  summary: string;
+}
+
+export interface AwardsStore {
+  awards: AwardItem[];
+  add: (newEducation: AwardItem) => void;
+  get: (index: number) => void;
+  remove: (index: number) => void;
+  set: (values: AwardItem[]) => void;
+  onmoveup: (index: number) => void;
+  onmovedown: (index: number) => void;
+}
