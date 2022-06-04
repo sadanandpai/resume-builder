60456df4e24a778d91e8880a8148cb78c9fe7e85
Siva K <sivakwalker3@gmail.com>
2022-06-04 18:15:13 +0530

    fix: 🐛 dayjs date parsing issue fix

--- src/helpers/utils/index.ts
+++ src/helpers/utils/index.ts
@@ -1,3 +1,10 @@
+import { Dayjs } from 'dayjs/index';
+
 export const sum = (a: number, b: number): number => {
   return a + b;
+};
+
+export const dateParser = (dateValue: string | Dayjs | null, outputFormat = 'MMM YYYY') => {
+  if (dateValue === null) return;
+  return typeof dateValue === 'object' ? dateValue.format(outputFormat) : dateValue;
 };
