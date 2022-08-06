c30f9330f4fc8cbafe0c1ecde4db4889137d7d57
sivak <sivakwalker3@gmail.com>
2022-08-06 08:52:44 +0530

    fix: 🐛 loading editor on demand to avoid self context dependency

--- src/helpers/common/components/richtext/index.tsx
+++ src/helpers/common/components/richtext/index.tsx
@@ -1,6 +1,5 @@
 import type { IJodit } from 'jodit/types';
 import { useRef, useEffect } from 'react';
-import { Jodit } from 'jodit';
 import 'jodit/build/jodit.min.css';
 
 import styles from './jodit.module.css';
@@ -17,62 +16,67 @@ export const RichtextEditor = ({ label, onChange, value }: IRichtext) => {
 
   useEffect(() => {
     if (editorRef.current) {
-      const editor = Jodit.make(editorRef.current, {
-        showCharsCounter: false,
-        showWordsCounter: false,
-        showXPathInStatusbar: false,
-        buttons: ['bold', 'italic', 'link', 'ul', 'ol', 'undo', 'redo'],
-        disablePlugins:
-          'add-new-line,print,preview,table,table-keyboard-navigation,select-cells,resize-cells,file,video,media,image,image-processor,image-properties,xpath,tab,stat,search,powered-by-jodit,mobile,justify,inline-popup,indent,iframe,fullsize',
-        useSearch: false,
-        askBeforePasteHTML: false,
-        askBeforePasteFromWord: false,
-        defaultActionOnPaste: 'insert_only_text',
-        maxHeight: 200,
-        link: {
-          formTemplate: (editor: IJodit) => {
-            const i18n = editor.i18n.bind(editor);
+      const initEditor = async () => {
+        const { Jodit } = await import('jodit');
+        const editor = Jodit.make(editorRef.current as HTMLTextAreaElement, {
+          showCharsCounter: false,
+          showWordsCounter: false,
+          showXPathInStatusbar: false,
+          buttons: ['bold', 'italic', 'link', 'ul', 'ol', 'undo', 'redo'],
+          disablePlugins:
+            'add-new-line,print,preview,table,table-keyboard-navigation,select-cells,resize-cells,file,video,media,image,image-processor,image-properties,xpath,tab,stat,search,powered-by-jodit,mobile,justify,inline-popup,indent,iframe,fullsize',
+          useSearch: false,
+          askBeforePasteHTML: false,
+          askBeforePasteFromWord: false,
+          defaultActionOnPaste: 'insert_only_text',
+          maxHeight: 200,
+          link: {
+            formTemplate: (editor: IJodit) => {
+              const i18n = editor.i18n.bind(editor);
 
-            return `<form class="jodit_form">
-              <div class="${styles.jodit_form_group}">
-                <label class="${styles['jodit-input-label']}" for="url">URL</label>
-                <input ref="url_input" class="${
-                  styles.jodit_input
-                }" required type="text" id="url" name="url" placeholder="http://" type="text"/>
-              </div>
-              <div ref="content_input_box" class="${styles.jodit_form_group}">
-              <label class="${styles['jodit-input-label']}" for="text">Text</label>
-                <input ref="content_input" class="${
-                  styles.jodit_input
-                }" id="text" required name="text" placeholder="${i18n('Text')}" type="text"/>
-              </div>
-              <label ref="target_checkbox_box">
-                <input ref="target_checkbox" class="jodit_checkbox" name="target" type="checkbox" checked/>
-                <span>${i18n('Open in new tab')}</span>
-              </label>
-              <label ref="nofollow_checkbox_box">
-                <input ref="nofollow_checkbox" class="jodit_checkbox" name="nofollow" type="checkbox" checked/>
-                <span>${i18n('No follow')}</span>
-              </label>
-              <div class="${styles.jodit_buttons}">
-                <button ref="unlink" class="${styles.jodit_button} ${
-              styles.jodit_unlink_button
-            }" type="button">${i18n('Unlink')}</button>
-                <button ref="insert" class="${styles.jodit_button} ${
-              styles.jodit_link_insert_button
-            }" type="submit">${i18n('Insert')}</button>
-              </div>
-            <form/>`;
+              return `<form class="jodit_form">
+                <div class="${styles.jodit_form_group}">
+                  <label class="${styles['jodit-input-label']}" for="url">URL</label>
+                  <input ref="url_input" class="${
+                    styles.jodit_input
+                  }" required type="text" id="url" name="url" placeholder="http://" type="text"/>
+                </div>
+                <div ref="content_input_box" class="${styles.jodit_form_group}">
+                <label class="${styles['jodit-input-label']}" for="text">Text</label>
+                  <input ref="content_input" class="${
+                    styles.jodit_input
+                  }" id="text" required name="text" placeholder="${i18n('Text')}" type="text"/>
+                </div>
+                <label ref="target_checkbox_box">
+                  <input ref="target_checkbox" class="jodit_checkbox" name="target" type="checkbox" checked/>
+                  <span>${i18n('Open in new tab')}</span>
+                </label>
+                <label ref="nofollow_checkbox_box">
+                  <input ref="nofollow_checkbox" class="jodit_checkbox" name="nofollow" type="checkbox" checked/>
+                  <span>${i18n('No follow')}</span>
+                </label>
+                <div class="${styles.jodit_buttons}">
+                  <button ref="unlink" class="${styles.jodit_button} ${
+                styles.jodit_unlink_button
+              }" type="button">${i18n('Unlink')}</button>
+                  <button ref="insert" class="${styles.jodit_button} ${
+                styles.jodit_link_insert_button
+              }" type="submit">${i18n('Insert')}</button>
+                </div>
+              <form/>`;
+            },
+            noFollowCheckbox: false,
+            openInNewTabCheckbox: false,
           },
-          noFollowCheckbox: false,
-          openInNewTabCheckbox: false,
-        },
-      });
-      editor.value = value;
-      editor.events.on('change', (htmlOutput) => {
-        onChange(htmlOutput);
-      });
+        });
+        editor.value = value;
+        editor.events.on('change', (htmlOutput) => {
+          onChange(htmlOutput);
+        });
+      };
+      initEditor();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
