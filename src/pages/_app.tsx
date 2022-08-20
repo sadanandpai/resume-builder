0e71442dddb77eb20fdb81d38c02ebf0d22ce447
Vivek <audit536@gmail.com>
2022-08-20 09:44:29 +0530

    feat: :sparkles: Add mouseflow analytics only on deployed URL
    Analytics will not be recorded on localhost

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,25 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { GLOBAL_MUI_THEME } from '../styles/global.theme';
 
 import '../styles/globals.css';
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    _mfq: any;
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Mouseflow analytics
+    window._mfq = window._mfq || [];
+    const mf = document.createElement('script');
+    mf.type = 'text/javascript';
+    mf.defer = true;
+    mf.src = '//cdn.mouseflow.com/projects/2f28becd-e1bc-4244-9fd4-7a9788c05b25.js';
+    document.getElementsByTagName('head')[0].appendChild(mf);
+  }, []);
+
   return (
     <ThemeProvider theme={GLOBAL_MUI_THEME}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
