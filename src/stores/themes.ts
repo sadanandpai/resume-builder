1b0e5f1e4018d0ac39c47904ee189278e4de77e1
sivak <sivakwalker3@gmail.com>
2022-09-03 18:03:27 +0530

    update:  theme store interface name change

--- src/stores/themes.ts
+++ src/stores/themes.ts
@@ -3,13 +3,13 @@ import create from 'zustand';
 import { SYSTEM_COLORS, CUSTOM_THEME_COLOR } from 'src/helpers/constants/index';
 import { ThemeColorIntf } from 'src/helpers/constants/index.interface';
 
-interface ThemeStore {
+interface IThemeStore {
   selectedTheme: ThemeColorIntf;
   customTheme: ThemeColorIntf;
   chooseTheme: (theme: ThemeColorIntf) => void;
 }
 
-export const useThemes = create<ThemeStore>((set) => ({
+export const useThemes = create<IThemeStore>((set) => ({
   selectedTheme: SYSTEM_COLORS[0],
   customTheme: CUSTOM_THEME_COLOR,
   chooseTheme: (theme: ThemeColorIntf) => {
