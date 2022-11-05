1e1c85229e1877acff5d93cc65062ef4b6efddde
sivak <sivakwalker3@gmail.com>
2022-11-05 16:59:23 +0530

    refactor: 🎨 removing toggle section temp change

--- src/modules/builder/editor/atoms/HeaderTitle.tsx
+++ src/modules/builder/editor/atoms/HeaderTitle.tsx
@@ -2,11 +2,11 @@ import Image from 'next/image';
 
 const HeaderTitle = ({ title }: { title: string }) => (
   <div className="flex items-center my-5 cursor-pointer">
-    <div className="mr-6 flex items-center">
+    {/* <div className="mr-6 flex items-center">
       <Image src="/icons/eye.svg" alt="eye" height="16px" width="16px" />
-    </div>
+    </div> */}
 
-    <p className="text-xl">{title}</p>
+    <p className="text-xl ml-2">{title}</p>
 
     <div className="ml-auto pl-4 flex items-center">
       <Image src="/icons/right-arrow.svg" alt="right-arrow" height="16px" width="16px" />
