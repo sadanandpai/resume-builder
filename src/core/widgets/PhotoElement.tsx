1fb5dfbabea518b0a558c6105a0e666c75984663
Sadanand Pai <sadanand.pai@coindcx.com>
2022-12-29 22:56:15 +0530

    Reset: All main files

--- src/core/widgets/PhotoElement.tsx
+++
@@ -1,7 +0,0 @@
-import { useItems } from 'src/stores/settings.store';
-
-export function PhotoElement({ children }: any) {
-  const isPhotoDisplayed = useItems((state: any) => state.isPhotoDisplayed);
-
-  return isPhotoDisplayed && children;
-}
--- src/templates/components/template3/shared/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as Section } from './Section/Section';
