b9d23fd095e306e62389f1ab76f42346691def89
Sadanand Pai <sadypai@gmail.com>
2023-01-17 22:34:16 +0530

    Update ProfileName.tsx

--- src/templates/modern/atoms/ProfileName.tsx
+++ src/templates/modern/atoms/ProfileName.tsx
@@ -1,7 +1,7 @@
 export const ProfileName = ({ name }: { name: string }) => {
   return (
     <h3
-      className="text-3xl font-medium max-w-[60%] overflow-hidden overflow-ellipsis whitespace-nowrap"
+      className="text-3xl font-medium max-w-[90%] overflow-hidden overflow-ellipsis whitespace-nowrap"
       title={name}
     >
       {name}
