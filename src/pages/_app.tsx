fe9e2272fb52c405a147a77c82f998e515ed88be
Sadanand Pai <sadanand.pai@coindcx.com>
2023-02-12 15:46:42 +0530

    fix: Mouseflow removal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,25 +8,8 @@ import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 import { GLOBAL_MUI_THEME } from '../styles/global.theme';
 import { LocalizationProvider } from '@mui/x-date-pickers';
-import { useEffect } from 'react';
-
-declare global {
-  interface Window {
-    _mfq: any;
-  }
-}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    // Mouseflow analytics
-    window._mfq = window._mfq || [];
-    const mf = document.createElement('script');
-    mf.type = 'text/javascript';
-    mf.defer = true;
-    mf.src = '//cdn.mouseflow.com/projects/2f28becd-e1bc-4244-9fd4-7a9788c05b25.js';
-    document.getElementsByTagName('head')[0].appendChild(mf);
-  }, []);
-
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={GLOBAL_MUI_THEME}>
