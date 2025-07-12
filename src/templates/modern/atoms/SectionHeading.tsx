8c82c2044dfc0ea1d97f0e64ae11903eaa02b1e0
Sadanand Pai <sadypai@gmail.com>
2025-07-12 23:50:43 +0530

    Modern template border color fix

--- src/templates/modern/atoms/SectionHeading.tsx
+++ src/templates/modern/atoms/SectionHeading.tsx
@@ -1,6 +1,6 @@
 export const SectionHeading = ({ title }: { title: string }) => {
   return (
-    <div className="relative mb-2 text-base font-medium before:content-[''] before:w-full before:border-b-4 before:border-color-[#b2b4b6] before:absolute before:-bottom-0.5">
+    <div className="relative mb-2 text-base font-medium before:content-[''] before:w-full before:border-b-4 before:border-[#e5e7eb] before:absolute before:-bottom-0.5">
       {title}
     </div>
   );
